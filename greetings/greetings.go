495a8c4b96f0d6cf48723810a5daa46083ccb752
Joe Lyles <lylesjoe@protonmail.com>
2024-07-25 20:29:42 -0400

    error handler and random logic

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,25 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func randomFormat() string {
+	formats := [] string {
+		"Hi, %v. Welcome!",
+		"Great to see you %v",
+		"Hey, %v, what do you know?",
+	}
+	return formats[rand.Intn(len(formats))]
 }
